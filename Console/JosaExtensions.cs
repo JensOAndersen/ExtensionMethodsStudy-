69943e45bd6cf27aa3b5108186a9a47ec395844d
JensOAndersen <jensoleand@gmail.com>
2019-06-12 09:16:37 +0200

    Updated with find in List

---
+++ Console/JosaExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public static class JosaExtensions
+    {
+        /// <summary>
+        /// Iterates through a tree-like data structure, and searches for a predicate match
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="child">Object with a child</param>
+        /// <param name="predicate">boolean match</param>
+        /// <param name="getChildren">get children of the object</param>
+        /// <returns>a match of the predicate</returns>
+        public static T FindWhereInChildren<T>(this T child, Func<T, bool> predicate, Func<T, IEnumerable<T>> getChildren)
+        {
+            if (predicate(child)) //predicate looks like: 'x => x.id == 1' or 'x => x.name == 'hansel'
+            {
+                return child;
+            }
+            else
+            {
+                foreach (var item in getChildren(child)) //getChildren is 'x => x.children
+                {
+                    return item.FindWhereInChildren(predicate, getChildren);
+                }
+            }
+
+            return default(T);
+            /*
+             The method is recursive as it needs to look through all children of the given object.
+             */
+        }
+
+        /// <summary>
+        /// Finds all matching occurances in a list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> WhereInList<T>(this IEnumerable<T> root, Func<T, bool> predicate)
+        {
+            foreach (var item in root)
+                if (predicate(item))
+                    yield return item;
+        }
+    }
+}
--- Console/Program.cs
+++ Console/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Console
+namespace ConsoleApp
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 200; i++)
+                numbers.Add(rnd.Next(1000, 10000));
+
+
+            var res = numbers.WhereInList(x => x > 5000);
+
+            Console.ReadKey();
         }
     }
 }
